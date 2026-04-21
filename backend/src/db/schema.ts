9f3cc7e0dcbf6b5095468925dab630dfa0eaefe1
6arcia_Lui5 <luisgarciavv7@gmail.com>
2026-04-21 03:53:24 -0700

    Set up drizzle ORM with PostgreSQL configuration and initial DB schema

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,76 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const users = pgTable("users", {
+    id: text("id").primaryKey(), // clerkID
+    email: text("email").notNull().unique(),
+    name: text("name").notNull(),
+    imageURL: text("image_url"),
+
+    createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const collections = pgTable("collections", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    imageURL: text("image_url"),
+    timePeriod: text("time_period").notNull(),
+
+    createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const records = pgTable("records", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    imageURL: text("image_url"),
+    date: text("date").notNull(),
+    material: text("material").notNull(),
+    dimensions: text("dimensions").notNull(),
+    classification: text("classification").notNull(),
+    credit: text("credit").notNull(),
+    objectNumber: text("object_number").notNull(),
+    userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade"}),
+    collectionId: uuid("collectionId").notNull().references(() => collections.id, { onDelete: "cascade"}),
+
+    createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+
+// Relations:
+
+// User can have many records
+export const userRelations = relations(users, ({many}) => ({
+    records: many(records)
+}));
+
+// Collection can have many records
+export const collectionRelations = relations(collections, ({many}) => ({
+    records: many(records),
+}));
+
+// Records can have one users and belong to one collection
+export const recordRelations = relations(records, ({one}) => ({
+    users: one(users, {
+        fields:[records.userId], 
+        references: [users.id]
+    }),
+    collections: one(collections, {
+        fields:[records.collectionId], 
+        references: [collections.id]})
+}));
+
+export type User = typeof users.$inferSelect;
+export type newUser = typeof users.$inferInsert;
+
+export type collection = typeof collections.$inferSelect;
+export type newCollection = typeof collections.$inferInsert;
+
+export type Record = typeof records.$inferSelect;
+export type newRecord = typeof records.$inferInsert;
